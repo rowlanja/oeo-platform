308886bee13ff291438b78c7526c4608c2f94aa2
james rowland <rowlanja@tcd.ie>
2022-04-23 19:22:07 +0100

    first smart contract interaction complete

---
+++ src/contracts/storage.js
@@ -0,0 +1,32 @@
+export const storage_abi = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "x",
+				"type": "uint256"
+			}
+		],
+		"name": "set",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "get",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+
+export const storage_address = "0x27f08a580c8b482D46CB3BAC088F2f23DC96A5a6"
