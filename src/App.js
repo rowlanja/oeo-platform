dfe1130f0a7c41968d9447bf032b08ba4fdcce41
james rowland <rowlanja@tcd.ie>
2022-04-23 14:18:52 +0100

    connecting to metamask done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import { ethers } from "ethers";
+import "./App.css";
+import { Button, Card } from "react-bootstrap";
+  
 function App() {
+  
+  // usetstate for storing and retrieving wallet details
+  const [data, setdata] = useState({
+    address: null,
+    Balance: null,
+  });
+  
+  // Button handler button for handling a
+  // request event for metamask
+  const btnhandler = () => {
+  
+    // Asking if metamask is already present or not
+    if (window.ethereum) {
+  
+      // res[0] for fetching a first wallet
+      window.ethereum
+        .request({ method: "eth_requestAccounts" })
+        .then((res) => accountChangeHandler(res[0]));
+    } else {
+      alert("install metamask extension!!");
+    }
+  };
+  
+  // getbalance function for getting a balance in
+  // a right format with help of ethers
+  const getbalance = (address) => {
+  
+    // Requesting balance method
+    window.ethereum
+      .request({ 
+        method: "eth_getBalance", 
+        params: [address, "latest"] 
+      })
+      .then((balance) => {
+        // Setting balance
+        setdata({
+          address: address,
+          Balance: ethers.utils.formatEther(balance),
+        });
+      });
+  };
+  
+  // Function for getting handling all events
+  const accountChangeHandler = (account) => {
+    console.log(account)
+
+    // Setting a balance
+    getbalance(account);
+  };
+  
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Calling all values which we 
+       have stored in usestate */}
+  
+      <Card className="text-center">
+        <Card.Header>
+          <strong>Address: </strong>
+          {data.address}
+        </Card.Header>
+        <Card.Body>
+          <Card.Text>
+            <strong>Balance: </strong>
+            {data.Balance}
+          </Card.Text>
+          <Button onClick={btnhandler} variant="primary">
+            Connect to wallet
+          </Button>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
-
+  
 export default App;
